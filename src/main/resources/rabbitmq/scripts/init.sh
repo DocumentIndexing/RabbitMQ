3df3fdf124a3bcb4373174baf166e25e8aa82153
Paul Hitchcock <paul.h.hitchcock@gmail.com>
2019-05-14 16:30:47 +0100

    Changes to use local images for Dev builds.

--- src/main/resources/rabbitmq/scripts/init.sh
+++ src/main/resources/rabbitmq/scripts/init.sh
@@ -31,4 +31,4 @@
 # $@ is used to pass arguments to the rabbitmq-server command.
 # For example if you use it like this: docker run -d rabbitmq arg1 arg2,
 # it will be as you run in the container rabbitmq-server arg1 arg2
-/docker-entrypoint.sh rabbitmq-server $@
+/usr/local/bin/docker-entrypoint.sh rabbitmq-server $@
